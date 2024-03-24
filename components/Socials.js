8dd01b518f0e21a1241fe9fc2e7e7034887d4798
TroyMoses <mugabimoses07@gmail.com>
2024-03-25 02:39:05 +0600

    Prototype done

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,52 @@
+import Link from "next/link";
+
+// Icons
+import {
+  RiYoutubeLine,
+  RiInstagramLine,
+  RiTwitterLine,
+  RiLinkedinLine,
+  RiDiscordLine,
+  RiGithubLine,
+} from 'react-icons/ri'
+
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div 
+      className="flex items-center gap-x-5 text-lg">
+      <Link 
+        href={''} 
+        className="hover:text-accent transition-all duration-300">
+          <RiGithubLine />
+      </Link>
+      <Link 
+        href={''} 
+        className="hover:text-accent transition-all duration-300">
+          <RiLinkedinLine />
+      </Link>
+      <Link 
+        href={''} 
+        className="hover:text-accent transition-all duration-300">
+          <RiTwitterLine />
+      </Link>
+      <Link 
+        href={''} 
+        className="hover:text-accent transition-all duration-300">
+          <RiYoutubeLine />
+      </Link>
+      <Link 
+        href={''} 
+        className="hover:text-accent transition-all duration-300">
+          <RiInstagramLine />
+      </Link>
+      <Link 
+        href={''} 
+        className="hover:text-accent transition-all duration-300">
+          <RiDiscordLine />
+      </Link>
+    </div>
+  ); 
 };
 
 export default Socials;
--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -23,8 +23,87 @@ const testimonialData = [
   },
 ];
 
+// Image
+import Image from 'next/image';
+
+// Import swiper react components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// Import required modules
+import { Navigation, Pagination } from 'swiper';
+
+// Icons
+import { FaQuoteLeft } from 'react-icons/fa';
+
 const TestimonialSlider = () => {
-  return <div>Testimonial Slider</div>;
+  return (
+    <Swiper 
+      navigation={true} 
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Navigation, Pagination]}
+      className='h-[400px]'
+    >
+      {testimonialData.map((person, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div 
+              className='flex flex-col items-center md:flex-row gap-x-8 h-full px-16'>
+              {/* Avatar, Name, Position */}
+              <div className='w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0'>
+                <div 
+                  className='flex flex-col justify-center text-center'>
+                  {/* Avatar */}
+                  <div className='mb-2 mx-auto'>
+                    <Image 
+                      src={person.image} 
+                      width={100} 
+                      height={100} 
+                      alt='' 
+                    />
+                  </div>
+                  {/* Name */}
+                  <div className='text-lg'>
+                    {person.name}
+                  </div>
+                  {/* Position */}
+                  <div 
+                    className='text-[12px] uppercase font-extralight tracking-widest'>
+                    {person.position}
+                  </div>
+                </div>
+              </div>
+              
+              {/* Quote $ Message */}
+              <div 
+                className='flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20'>
+                {/* Quote icon */}
+                <div className='mb-4'>
+                  <FaQuoteLeft 
+                    className='text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0' />
+                </div>
+                {/* Message */}
+                <div 
+                  className='xl:text-lg text-center md:text-left'>{person.message}</div>
+              </div>
+              
+
+
+            </div>
+            
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default TestimonialSlider;
+
+
